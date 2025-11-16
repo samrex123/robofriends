7f2cdd015a4800c37ea5f2d52c5818105e7c674b
Samuel Rexborg <sam.rex95@gmail.com>
2025-11-16 19:15:34 +0100

    Add Scroll component and update App to use it for better layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
+import Scroll from "./Scroll";
 import { robots } from "./robots";
 import './App.css';
 
@@ -8,9 +9,15 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ robots: users }));
     }
 
     onSearchChange = (event) => {
@@ -25,7 +32,9 @@ class App extends Component {
       <div className="tc">
           <h1 className="f1">RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
+          <Scroll >
           <CardList robots={filteredRobots} />
+          </Scroll>
     </div>
   );
 }
