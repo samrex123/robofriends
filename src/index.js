cf94ad2d525dd966c4786c0bdafdbee15352c0c2
Samuel Rexborg <sam.rex95@gmail.com>
2025-11-16 20:18:47 +0100

    Refactor project structure: move components to a dedicated folder and update imports

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'tachyons/css/tachyons.min.css';
-import App from './App';
+import App from './containers/App';
 import { robots } from './robots';
 import reportWebVitals from './reportWebVitals';
 
